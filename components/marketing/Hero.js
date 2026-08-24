1dfb1dab94c956c62bc7ec6ea356db7995b36454
Wealthstein Adekoya <wealthsteinadekoya@gmail.com>
2026-08-24 17:59:35 +0100

    Update Hero.js

--- components/marketing/Hero.js
+++ components/marketing/Hero.js
@@ -85,9 +85,6 @@ export default function Hero({ content, audience, onPrimaryCta }) {
               </Box>
             ))}
           </Stack>
-          <Typography variant="caption" sx={{ color: tokens.muted, display: "block", mt: 2 }}>
-            Illustrative example - not yet reflective of live usage.
-          </Typography>
         </Box>
       </Container>
     </Box>
